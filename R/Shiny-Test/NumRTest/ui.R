adb0a809c99fa5fc75e3d1cab31f615c0b49e0a3
eibrahi <elvir.ibrahimovic@gmail.com>
2019-10-11 20:52:11 +0200

    Shiny commited!

--- R/Shiny-Test/NumRTest/ui.R
+++ R/Shiny-Test/NumRTest/ui.R
@@ -13,7 +13,7 @@ library(shiny)
 shinyUI(fluidPage(
 
     # Application title
-    titlePanel("Hau in die Tasten Elvir"),
+    titlePanel("Hau in die Tasten Elvir und Svetislav"),
 
     # Sidebar with a slider input for number of bins
     sidebarLayout(
